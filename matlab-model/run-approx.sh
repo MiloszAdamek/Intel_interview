65f1a641ea2d0ff2f532239e26a4e1ecf875ad22
Milosz <miloush00767@gmail.com>
2025-04-12 11:21:30 +0200

    Initial commit

---
+++ matlab-model/run-approx.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [ -f coeffs.txt ]; then
+    rm coeffs.txt
+fi
+
+octave approx.m | tee coeffs.txt
+
+# optionally, use optimized coeffs:
+#octave approx_opt.m | tee coeffs.txt
+
+octave print_coeffs.m | tee hex_coeffs.txt
+
+
+
